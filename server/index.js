724d3f84b7dc56053b272180375edab1f69397a0
Aidan Brown <aidanbrownn87@gmail.com>
2021-05-10 22:48:44 +0100

    adds graphql to express

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const path = require('path');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const PORT = process.env.PORT || 3001;
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+ 
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
